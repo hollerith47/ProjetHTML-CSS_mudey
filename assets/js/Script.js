984ba061f5125b0b14424070077b379b3f8bcbf3
Hollerith47 <hermanhmakiese@gmail.com>
2022-12-27 18:34:08 +0300

    quelques ajustement responsive

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,18 +1,29 @@
 window.onload = () => {
-	const MenuIcon = document.querySelector('.icon-menu')
-	const NavBar = document.querySelector(".navBar");
-	
-	if (MenuIcon){
-		MenuIcon.addEventListener('click', () => {
-			console.log('icon-menu')
-			if (NavBar.style.display === 'block'){
-				NavBar.style.display = 'none';
-			}else {
-				NavBar.style.display = "block";
-			}
-			// NavBar.style.display = 'none';
-			
-		})
-	}
-	
-}
+  const MenuIcon = document.querySelector(".icon-menu");
+  const NavBar = document.querySelector(".navBar");
+  const links = document.querySelectorAll(".nav-item");
+
+  if (MenuIcon) {
+    MenuIcon.addEventListener("click", () => {
+      console.log("icon-menu");
+      if (NavBar.style.display === "block") {
+        NavBar.style.display = "none";
+      } else {
+        NavBar.style.display = "block";
+      }
+      // NavBar.style.display = 'none';
+    });
+  }
+
+  if (links.length) {
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        links.forEach((link) => {
+          link.classList.remove("active");
+        });
+        e.preventDefault();
+        link.classList.add("active");
+      });
+    });
+  }
+};
